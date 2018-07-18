08d6f6b372aabef890c7d70a0b895b60fe333893
xhyumiracle <xhyumiracle@gmail.com>
2018-07-18 11:55:27 +0800

    fixed kali script bugs

--- vbox-xhyu.sh
+++ vbox-xhyu.sh
@@ -1,16 +1,24 @@
-
-if [ "$1" == 'off' ]
+# if (( $# > 0 )) # isn't supported by POSIX shell
+if [ $# -gt 0 ]
 then
-  VBoxManage controlvm "OffsecVM-2017.1-i686" poweroff --type headless
-elif [ "$1" == 'pause' ]
-then
-  VBoxManage controlvm "OffsecVM-2017.1-i686" pause --type headless
-elif [ "$1" == 'resume' ]
-then
-  VBoxManage controlvm "OffsecVM-2017.1-i686" resume --type headless
-elif [ "$1" == 'status' ]
-then
-  echo `vboxmanage showvminfo "OffsecVM-2017.1-i686" | grep -c "running (since"`
+    if [ "$1" == 'off' ]
+    then
+      VBoxManage controlvm "OffsecVM-2017.1-i686" poweroff --type headless
+    elif [ "$1" == 'pause' ]
+    then
+      VBoxManage controlvm "OffsecVM-2017.1-i686" pause --type headless
+    elif [ "$1" == 'resume' ]
+    then
+      VBoxManage controlvm "OffsecVM-2017.1-i686" resume --type headless
+    elif [ "$1" == 'status' ]
+    then
+      echo `vboxmanage showvminfo "OffsecVM-2017.1-i686" | grep -c "running (since"`
+    elif [ "$1" == 'edit' ]
+    then
+      vim ${0}
+    else
+      echo 'Usage: kali [off/pause/resume/status/edit]'
+    fi
 else
   is_running=`vboxmanage showvminfo "OffsecVM-2017.1-i686" | grep -c "running (since"`
   if [ "$is_running" == '0' ] 
